a32dd42c99815f2dbe0da3ff040ca668fa78a58f
niyobern <niyobern@outlook.com>
2023-01-10 07:43:39 +0200

    done

--- database/models.py
+++ database/models.py
@@ -44,11 +44,11 @@ class Bonus(Base):
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, nullable=False)
-    email: Column(String, nullable=False, unique=True)
-    phone: Column(String, nullable=False, unique=True)
-    password: Column(String, nullable=False)
-    active: Column(Boolean)
-    role: Column(String)
+    email = Column(String, nullable=False, unique=True)
+    phone = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    active = Column(Boolean)
+    role = Column(String)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
 
 class Leave(Base):
