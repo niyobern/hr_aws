44de242a9f54b58e3a99e74a198be43753135ab1
niyobern <niyobern@outlook.com>
2023-01-12 06:08:20 +0200

    done

--- routes/users.py
+++ routes/users.py
@@ -67,7 +67,7 @@ def modify_user(employee: schemas.EmployeeUpdate, db: Session = Depends(get_db),
     db.commit()
     user_query = db.query(models.User).filter(models.User.id == employee.user_id)
     user_query.update({"role": role}, synchronize_session=False)
-    announcement = db.query(models.Announcement).filter(models.Announcement.id_in_table == employee.id).filter(models.Announcement.table == "employees")
+    announcement = db.query(models.Announcement).filter(models.Announcement.id_in_table == employee.id).filter(models.Announcement.table == "employees").first()
     announcement.update({"seen": True}, synchronize_session=False) 
     return JSONResponse(status_code=200, content={"message": "Finished"})
 
