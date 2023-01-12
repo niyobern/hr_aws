22e0e0ee8564043928d81d835c0c866b82f821da
niyobern <niyobern@outlook.com>
2023-01-12 20:11:07 +0200

    done

--- routes/users.py
+++ routes/users.py
@@ -27,7 +27,7 @@ def create_user(employee: schemas.Employee, db: Session = Depends(get_db), curre
 @router.get('/new')
 def return_get_all_users(db: Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     if current_user.role == "hr":
-        announcements = db.query(models.Announcement).filter(and_(models.Announcement.seen == None), models.Announcement.table == "employees").all()
+        announcements = db.query(models.Announcement).filter(and_(models.Announcement.seen == None, models.Announcement.table == "employees")).all()
         employees = []
         for i in announcements:
             employee = db.query(models.Employee).filter(models.Employee.id == i.id_in_table).first()
@@ -66,7 +66,7 @@ def modify_user(employee: schemas.EmployeeUpdate, db: Session = Depends(get_db),
     db.commit()
     user_query = db.query(models.User).filter(models.User.id == employee.user_id)
     user_query.update({"role": role}, synchronize_session=False)
-    announcement = db.query(models.Announcement).filter(models.Announcement.id_in_table == employee.id).filter(models.Announcement.table == "employees").first()
+    announcement = db.query(models.Announcement).filter(and_(models.Announcement.id_in_table == employee.id, models.Announcement.table == "employees")).first()
     query = db.query(models.Announcement).filter(models.Announcement.id == announcement.id)
     query.update({"seen": True}, synchronize_session=False) 
     return JSONResponse(status_code=200, content={"message": "Finished"})
