f5dfa892e9f6f2b475a9a2c5b08e7b30ddbe7c1a
niyobern <niyobern@outlook.com>
2023-01-15 23:12:40 +0200

    done

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
 web: gunicorn main:app --workers=4 --worker-class=uvicorn.workers.UvicornWorker
+revision: alembic revision --autogenerate
+upgrade: alembic upgrade head
---
+++ ocr.py
@@ -0,0 +1,17 @@
+# text recognition
+import cv2
+import pytesseract
+
+# read image
+img = cv2.imread('quotes.jpg')
+
+# configurations
+config = ('-l eng --oem 1 --psm 3')
+
+# pytessercat
+pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
+text = pytesseract.image_to_string(img, config=config)
+
+# print text
+text = text.split('\n')
+print(text)
